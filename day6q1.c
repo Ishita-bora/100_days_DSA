a8951d0f1a64d0592e9e34aa2f4f1a5e3e7b9d63
ishita bora <ishitabora20@gmail.com>
2026-02-09 21:46:51 +0530

    Add files via upload

---
+++ day6q1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    if(n == 0) {
+        return 0;
+    }
+
+    int i = 0;
+
+    for(int j = 1; j < n; j++) {
+        if(a[j] != a[i]) {
+            i++;
+            a[i] = a[j];
+        }
+    }
+
+    for(int k = 0; k <= i; k++) {
+        printf("%d ", a[k]);
+    }
+
+    return 0;
+}
