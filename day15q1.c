e3aa5861068e3323761733003fc090d402afeb63
ishita bora <ishitabora20@gmail.com>
2026-02-15 19:41:07 +0530

    Add files via upload

---
+++ day15q1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int m, n, i, j;
+    int a[100][100];
+    int sum = 0;
+
+    scanf("%d %d", &m, &n);
+
+    for(i = 0; i < m; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int limit = (m < n) ? m : n;
+
+    for(i = 0; i < limit; i++) {
+        sum = sum + a[i][i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
