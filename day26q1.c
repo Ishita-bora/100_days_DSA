91ae670d0e2e089e70f3aeb2cdfb978700359355
ishita bora <ishitabora20@gmail.com>
2026-02-26 19:00:28 +0530

    Add files via upload

---
+++ day26q1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node 
+{
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+
+int main() 
+{
+    int n, i, val;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%d", &val);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = val;
+        newNode->next = NULL;
+        newNode->prev = NULL;
+
+        if (head == NULL) 
+        {
+            head = newNode;
+            temp = newNode;
+        } else 
+        {
+            temp->next = newNode;
+            newNode->prev = temp;
+            temp = newNode;
+        }
+    }
+
+    temp = head;
+    while (temp != NULL) 
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
