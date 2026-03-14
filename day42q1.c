52163cdbbb49ea84c647e20e1a29f00e3c1bd9e8
ishita bora <ishitabora20@gmail.com>
2026-03-14 21:56:31 +0530

    Reverse integer queue using stack implementation
    Implemented a method to reverse the order of elements in a queue by utilizing a stack. The program reads integers into a queue, reverses them, and then outputs the reversed queue.

---
+++ day42q1.c
@@ -0,0 +1,61 @@
+Given a queue of integers, reverse the queue using a stack.
+
+
+
+#include <stdio.h>
+#define MAX 100
+
+int queue[MAX], front = -1, rear = -1;
+int stack[MAX], top = -1;
+
+void enqueue(int x)
+{
+    if(rear == MAX - 1)
+        return;
+
+    if(front == -1)
+        front = 0;
+
+    queue[++rear] = x;
+}
+
+int dequeue()
+{
+    if(front == -1 || front > rear)
+        return -1;
+
+    return queue[front++];
+}
+
+void push(int x)
+{
+    stack[++top] = x;
+}
+
+int pop()
+{
+    return stack[top--];
+}
+
+int main()
+{
+    int n, x;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    while(front <= rear)
+        push(dequeue());
+
+    while(top != -1)
+        enqueue(pop());
+
+    while(front <= rear)
+        printf("%d ", dequeue());
+
+    return 0;
+}
