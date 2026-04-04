f4b51bd1774abe9f9818a0d055273901a797a9d5
ishita bora <ishitabora20@gmail.com>
2026-04-04 23:56:54 +0530

    Add files via upload

---
+++ day63.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int adj[MAX][MAX];
+int visited[MAX];
+int n;
+
+void dfs(int node) {
+    visited[node] = 1;
+    printf("%d ", node);
+
+    for (int i = 0; i < n; i++) {
+        if (adj[node][i] == 1 && visited[i] == 0) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    int edges, u, v, start;
+
+    scanf("%d", &n);
+    scanf("%d", &edges);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            adj[i][j] = 0;
+        }
+        visited[i] = 0;
+    }
+
+    for (int i = 0; i < edges; i++) {
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+
+    scanf("%d", &start);
+
+    dfs(start);
+
+    return 0;
+}
