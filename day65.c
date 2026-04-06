28e77fc918828abe303b5989d6a17d208bb16586
ishita bora <ishitabora20@gmail.com>
2026-04-06 22:13:56 +0530

    Add files via upload

---
+++ day65.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int visited[100];
+
+int dfs(int v, int parent, int adj[100][100], int n)
+{
+    visited[v] = 1;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(adj[v][i] == 1)
+        {
+            if(visited[i] == 0)
+            {
+                if(dfs(i, v, adj, n))
+                    return 1;
+            }
+            else if(i != parent)
+                return 1;
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int adj[100][100];
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            scanf("%d", &adj[i][j]);
+
+    for(int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    int cycle = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(visited[i] == 0)
+        {
+            if(dfs(i, -1, adj, n))
+            {
+                cycle = 1;
+                break;
+            }
+        }
+    }
+
+    if(cycle)
+        printf("YES");
+    else
+        printf("NO");
+
+    return 0;
+}
