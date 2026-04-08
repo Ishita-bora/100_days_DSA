c5c2b77bcb5f0cdd4610eafc619122c702aad1d8
ishita bora <ishitabora20@gmail.com>
2026-04-08 20:50:42 +0530

    Add files via upload

---
+++ day67.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int graph[MAX][MAX];
+int visited[MAX];
+int stack[MAX];
+int top = -1;
+int n;
+
+void dfs(int node) {
+    visited[node] = 1;
+
+    for (int i = 0; i < n; i++) {
+        if (graph[node][i] && !visited[i]) {
+            dfs(i);
+        }
+    }
+
+    stack[++top] = node;
+}
+
+void topologicalSort() {
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+        }
+    }
+
+    printf("Topological Order:\n");
+    while (top != -1) {
+        printf("%d ", stack[top--]);
+    }
+}
+
+int main() {
+    int edges, u, v;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+        for (int j = 0; j < n; j++) {
+            graph[i][j] = 0;
+        }
+    }
+
+    printf("Enter number of edges: ");
+    scanf("%d", &edges);
+
+    printf("Enter edges (u v):\n");
+    for (int i = 0; i < edges; i++) {
+        scanf("%d %d", &u, &v);
+        graph[u][v] = 1;
+    }
+
+    topologicalSort();
+
+    return 0;
+}
