d6f9c2f5490a17985a9f388bf08ea7ed12d87d49
ishita bora <ishitabora20@gmail.com>
2026-04-10 23:28:42 +0530

    Add files via upload

---
+++ day68.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int adj[MAX][MAX] = {0};
+    int indegree[MAX] = {0};
+
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        indegree[v]++;
+    }
+
+    int queue[MAX], front = 0, rear = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(indegree[i] == 0) {
+            queue[rear++] = i;
+        }
+    }
+
+    int topo[MAX], k = 0;
+
+    while(front < rear) {
+        int node = queue[front++];
+        topo[k++] = node;
+
+        for(int i = 0; i < n; i++) {
+            if(adj[node][i] == 1) {
+                indegree[i]--;
+                if(indegree[i] == 0) {
+                    queue[rear++] = i;
+                }
+            }
+        }
+    }
+
+    if(k != n) {
+        printf("Cycle detected");
+    } else {
+        for(int i = 0; i < k; i++) {
+            printf("%d ", topo[i]);
+        }
+    }
+
+    return 0;
+}
