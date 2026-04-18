bed060e9e93df88c05c75ad453b5d506417973b1
ishita bora <ishitabora20@gmail.com>
2026-04-18 23:56:54 +0530

    candidate voting task
    Added problem statement for vote counting challenge.

--- day74.c
+++ day74.c
@@ -1,3 +1,8 @@
+Problem Statement
+Given an array of candidate names where each name represents a vote cast for that candidate, determine the candidate who received the maximum number of votes. In case of a tie, return the lexicographically smallest candidate name.
+
+
+
 #include <stdio.h>
 #include <string.h>
 
