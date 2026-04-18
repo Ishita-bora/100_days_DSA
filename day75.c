ad8a6cb0d0edafdab545a26bcdfc457a3fe345a2
ishita bora <ishitabora20@gmail.com>
2026-04-18 23:59:31 +0530

    zero-sum subarray challenge
    Added problem statement for finding longest zero-sum subarray.

--- day75.c
+++ day75.c
@@ -1,3 +1,9 @@
+Problem Statement
+Given an array of integers containing both positive and negative values, find the length of the longest contiguous subarray whose sum is equal to zero.
+
+
+    
+
 #include <stdio.h>
 
 int main() {
