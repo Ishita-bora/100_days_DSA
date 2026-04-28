8b188b8ebc5d108d4a7ccef705e95b07bf093ca0
ishita bora <ishitabora20@gmail.com>
2026-04-28 23:09:36 +0530

    Add files via upload

---
+++ day86.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int integerSquareRoot(int n) {
+    int low = 0, high = n, ans = 0;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (mid * mid == n)
+            return mid;
+        else if (mid * mid < n) {
+            ans = mid;
+            low = mid + 1;
+        } else
+            high = mid - 1;
+    }
+
+    return ans;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    printf("%d", integerSquareRoot(n));
+
+    return 0;
+}
