491a01b2c7463724ac7e134399f900c697a40c97
ishita bora <ishitabora20@gmail.com>
2026-04-29 22:31:18 +0530

    cow placement challenge

--- day88.c
+++ day88.c
@@ -1,3 +1,5 @@
+Problem Statement Given n stalls located at different positions along a straight line and k cows, place the cows in the stalls such that the minimum distance between any two cows is maximized.
+
 #include <stdio.h>
 #include <stdlib.h>
 
